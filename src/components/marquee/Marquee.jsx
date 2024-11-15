f401eacdb7032a3b53846ab5a20c04e8d4cce8c8
Beukot <michalek7790@gmail.com>
2024-11-16 00:25:16 +0100

    first commit; including marquee

---
+++ src/components/marquee/Marquee.jsx
@@ -0,0 +1,27 @@
+import "/src/components/marquee/Marquee.css"
+
+function Marquee({images, direction, width, speed, gap, marginTop})
+{
+    return(
+        <div>
+            <div className={"image_board"} style={{
+                "--imageQuantity": images.length,
+                "--direction": direction,
+                "--imageWidth": width,
+                "--speed": speed,
+                "--imageGap": gap,
+                "--marginTop": marginTop,
+            }}>
+                <div className={"image_track"}>
+                    {images.map((image, index) => (
+                        <div key={index} className={"image"} style={{"--imageIndex": index + 1}}>
+                            <img src={image} alt={"marquee image"} width={width} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Marquee;
---
+++ src/main.jsx
@@ -0,0 +1,55 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Marquee from "./components/marquee/Marquee.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <div className={"divContainer"}>
+          <div className={"divColumn"}>
+              xd
+          </div>
+          <div className={"divColumn"}>
+              xd
+          </div>
+          <div className={"divColumn"}>
+              xd
+          </div>
+      </div>
+
+      {/*SPONSORS*/}
+      <>
+          <Marquee
+              images={[
+                  "./src/assets/sponsors/sponsors_bg_1.png",
+                  "./src/assets/sponsors/sponsors_bg_2.jpg",
+                  "./src/assets/sponsors/sponsors_bg_3.png",
+                  "./src/assets/sponsors/sponsors_bg_4.png",
+                  "./src/assets/sponsors/sponsors_bg_1.png",
+                  "./src/assets/sponsors/sponsors_bg_2.jpg",
+                  "./src/assets/sponsors/sponsors_bg_3.png",
+                  "./src/assets/sponsors/sponsors_bg_4.png",
+              ]}
+              direction={"forwards"}
+              width={"400px"}
+              speed={"15s"}
+              gap={"0px"}
+          />
+          <Marquee
+              images={[
+                  "./src/assets/sponsors/sponsors_logo_1.svg",
+                  "./src/assets/sponsors/sponsors_logo_2.png",
+                  "./src/assets/sponsors/sponsors_logo_3.png",
+                  "./src/assets/sponsors/sponsors_logo_1.svg",
+                  "./src/assets/sponsors/sponsors_logo_2.png",
+                  "./src/assets/sponsors/sponsors_logo_3.png",
+              ]}
+              direction={"reverse"}
+              width={"400px"}
+              speed={"15s"}
+              gap={"400px"}
+              marginTop={"-406px"}
+          />
+      </>
+  </StrictMode>,
+)
