bab2ae37e509c8d02e18d1c961488951666cdf07
4o4fra <franio.grzywaczewski@gmail.com>
2024-11-16 18:46:52 +0100

    :art: Rename method

--- api/src/utils/getCurrentEventMultiplier.ts
+++ api/src/utils/getCurrentEventMultiplier.ts
@@ -1,16 +1,17 @@
 import dbPromise from '../db/db';
 
-const getCurrentEvent = async (): Promise<number> => {
+const getCurrentEventMultiplier = async (): Promise<number> => {
     const db = await dbPromise;
     const now = new Date().toISOString();
     const event = await db.get(`
         SELECT plutaMultiplier
         FROM events
-        WHERE date_start <= ? AND date_end >= ?
+        WHERE date_start <= ?
+          AND date_end >= ?
         LIMIT 1
     `, [now, now]);
 
     return event ? event.plutaMultiplier : 1;
 };
 
-export default getCurrentEvent;
+export default getCurrentEventMultiplier;
