1a4a14bfdf399ee19cf33d52b7dc8e470ab0e4b1
4o4fra <franio.grzywaczewski@gmail.com>
2024-11-22 23:51:01 +0100

    :ambulance: Fix sponsor display on firefox

--- web/src/components/marquee/Marquee.jsx
+++ web/src/components/marquee/Marquee.jsx
@@ -1,27 +1,24 @@
-import "/src/components/marquee/Marquee.css"
+import "./Marquee.css"
 
-function Marquee({images, direction, width, speed, gap, marginTop})
-{
-    return(
-        <div>
-            <div className={"image_board"} style={{
-                "--imageQuantity": images.length,
-                "--direction": direction,
-                "--imageWidth": width,
-                "--speed": speed,
-                "--imageGap": gap,
-                "--marginTop": marginTop,
-            }}>
-                <div className={"image_track"}>
-                    {images.map((image, index) => (
-                        <div key={index} className={"image"} style={{"--imageIndex": index + 1}}>
-                            <img src={image} alt={"marquee image"} width={width} />
-                        </div>
-                    ))}
-                </div>
+function Marquee({images, direction, width, speed, gap, marginTop}) {
+    return (
+        <div className={"image_board"} style={{
+            "--imageQuantity": images.length,
+            "--direction": direction === "reverse" ? "reverse" : "normal",
+            "--imageWidth": width,
+            "--speed": speed,
+            "--imageGap": gap,
+            "--marginTop": marginTop,
+        }}>
+            <div className={"image_track"}>
+                {images.concat(images).map((image, index) => (
+                    <div key={index} className={"image"}>
+                        <img src={image} alt={"marquee image"} width={width}/>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Marquee;
