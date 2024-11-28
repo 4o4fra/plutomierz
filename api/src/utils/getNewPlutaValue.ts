4951240871fdb92be62281a18389f69b3eff79c4
Danrejk <114954056+Danrejk@users.noreply.github.com>
2024-11-28 13:20:57 +0000

    time bonus rework

--- api/src/utils/getNewPlutaValue.ts
+++ api/src/utils/getNewPlutaValue.ts
@@ -1,5 +1,6 @@
 import getWeatherData from "./getWeatherData";
 import getCurrentEventMultiplier from "./getCurrentEventMultiplier";
+import { group, time } from "console";
 
 const getNewPlutaValue = async (latitude: number, longitude: number) => {
     const weatherData = await getWeatherData(latitude, longitude);
@@ -14,8 +15,6 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
         snowfall = 0,
         weather_code: weatherCode = 0,
         cloud_cover: cloudCover = 0,
-        pressure_msl: pressureMsl = 0,
-        surface_pressure: surfacePressure = 0,
         wind_speed_10m: windSpeed10m = 0,
         wind_direction_10m: windDirection10m = 0,
         wind_gusts_10m: windGusts10m = 0
@@ -24,16 +23,17 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
     const month = now.getMonth();
 
-    const hourFactor = (hours >= 4 && hours < 8) ? -10 : (hours >= 12 && hours < 18) ? 12 : (hours >= 18 && hours < 24) ? 15 : 0;
+    const timeMultiplier = 20
+    const timeFactor = calcTimeFactor(hours,minutes) * timeMultiplier
+
     const monthFactor = (month >= 2 && month <= 4) ? 5 : (month >= 5 && month <= 7) ? 10 : (month >= 8 && month <= 10) ? 8 : 2;
     const temperatureFactor = (temperature > 20) ? 15 : (temperature > 10 || temperature < 0) ? 10 : -10;
     const precipitationFactor = (precipitation > 0) ? -10 : (cloudCover < 20) ? 10 : 2;
     const humidityFactor = (relativeHumidity > 80) ? 0 : (relativeHumidity < 30) ? 3 : 2;
     const windSpeedFactor = (windSpeed10m > 10) ? -10 : (windSpeed10m < 3) ? 10 : 2;
-    const pressureMslFactor = (pressureMsl > 1015) ? 5 : (pressureMsl < 1000) ? 3 : 2;
-    const surfacePressureFactor = (surfacePressure > 1015) ? 10 : (surfacePressure < 1000) ? 6 : 4;
     const apparentTemperatureFactor = (apparentTemperature > 25) ? 10 : (apparentTemperature < 0) ? 2 : -4;
     const dayFactor = isDay ? 6 : 0;
     const rainFactor = rain > 0 ? -10 : 8;
@@ -48,7 +48,37 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
 
     const eventMultiplier = await getCurrentEventMultiplier();
 
-    return eventMultiplier * weekendMultiplier * shortcutMultiplier * (hourFactor + monthFactor + temperatureFactor + precipitationFactor + humidityFactor + windSpeedFactor + pressureMslFactor + surfacePressureFactor + apparentTemperatureFactor + dayFactor + rainFactor + showersFactor + snowfallFactor + weatherCodeFactor + windDirectionFactor + windGustsFactor);
+    return eventMultiplier * weekendMultiplier * shortcutMultiplier * (timeFactor + monthFactor + temperatureFactor + precipitationFactor + humidityFactor + windSpeedFactor + apparentTemperatureFactor + dayFactor + rainFactor + showersFactor + snowfallFactor + weatherCodeFactor + windDirectionFactor + windGustsFactor);
 };
+
+const calcTimeFactor = (hour: number, minute: number): number => {
+    const godzina: number =  Number(String(hour)+String(minute))
+    let factor: number = 0
+
+    let przerwyBezDlugiej = [[805,815],[900,910],[955,1005],[1050,1100],[1250,1300],
+                            [1345,1355],[1440,1450],[1535,1545]]
+
+    // Między 11:45 a 12:00 jest max bonus
+    if(1145<=godzina && godzina<=1200){
+        factor = 1
+    }
+
+    // 10min przed i po długą przerwą mają duży bonus
+    else if((1135<godzina && godzina<1145) || (1200<godzina && godzina<1215)){
+        factor = 0.5
+    }
+
+    // bonus pluty na innych przerwach. Taki mniejszy
+    else{
+        for(const przerwa of przerwyBezDlugiej){
+            if(przerwa[0] <= godzina && godzina <= przerwa[1]){
+                factor = 0.25
+                return factor
+            }
+        }
+    }
+
+    return factor
+}
 
 export default getNewPlutaValue;
