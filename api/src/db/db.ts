7606766f0cd82c1c117266eeedf405af9bf81684
4o4fra <franio.grzywaczewski@gmail.com>
2024-11-29 22:40:03 +0100

    :rocket: Docker use volume for db to avoid losing it after updates

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -1,8 +1,11 @@
 import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+import {open} from 'sqlite';
+import path from 'path';
+
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, 'pluta.sqlite');
 
 const dbPromise = open({
-    filename: './pluta.sqlite',
+    filename: dbPath,
     driver: sqlite3.Database
 });
 
