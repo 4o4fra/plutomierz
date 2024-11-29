b353f4960fa8aa532fe6a21bc940b5535cf94be4
4o4fra <franio.grzywaczewski@gmail.com>
2024-11-30 00:19:46 +0100

    :ambulance: Dont throw the erro just log

--- api/src/utils/getWeatherData.ts
+++ api/src/utils/getWeatherData.ts
@@ -6,8 +6,7 @@ const getWeatherData = async (latitude: number, longitude: number) => {
         const response = await axios.get(url);
         return response.data;
     } catch (error) {
-        console.error("Error fetching weather data:", error);
-        throw error;
+        console.error("Error fetching weather data.");
     }
 };
 
