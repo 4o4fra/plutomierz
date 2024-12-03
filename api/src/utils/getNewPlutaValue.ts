f2ad63e2725b632a825bf733a2d8aa60e13fdf7a
4o4fra <franio.grzywaczewski@gmail.com>
2024-12-03 17:47:05 +0100

    :art: Replace an API with npm package

--- api/src/utils/getNewPlutaValue.ts
+++ api/src/utils/getNewPlutaValue.ts
@@ -19,13 +19,13 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
         uv_index_clear_sky: uvIndex = 0
     } = weatherData.current || {};
 
-    const now = await getTimeAtPluta(latitude, longitude);
+    const now = await getTimeAtPluta();
 
     // breaks
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const timeMultiplier = 20
-    const timeBonus = calcTimeFactor(hours,minutes) * timeMultiplier
+    const timeBonus = calcTimeFactor(hours, minutes) * timeMultiplier
 
     // days
     const day = now.getDay();
@@ -33,7 +33,7 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
     const dayBonus = calcDayFactor(day) * dayMultiplier
 
     // months
-    const month = now.getMonth()+1; // +1 because months are 0-indexed
+    const month = now.getMonth() + 1; // +1 because months are 0-indexed
     const monthMultiplier = 5
     const monthBonus = calcMonthFactor(month) * monthMultiplier
 
@@ -43,15 +43,15 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
 
     // uv index
     const uvIndexMultiplier = 10
-    const uvIndexBonus = (-((uvIndex/3.5)-1)*((uvIndex/3.5)-1) + 1) * uvIndexMultiplier
+    const uvIndexBonus = (-((uvIndex / 3.5) - 1) * ((uvIndex / 3.5) - 1) + 1) * uvIndexMultiplier
 
     // rain
     const rainMultiplier = 10
-    const rainBonus = (rain > 1 ? 0 : 1-rain) * rainMultiplier;
+    const rainBonus = (rain > 1 ? 0 : 1 - rain) * rainMultiplier;
 
     // shower
     const showersMultiplier = 5
-    const showersBonus = (showers > 1 ? 0 : 1-showers) * showersMultiplier;
+    const showersBonus = (showers > 1 ? 0 : 1 - showers) * showersMultiplier;
 
     // snow
     const snowMultiplier = 10;
@@ -63,19 +63,19 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
 
     // easter egg, temperature anomaly
     const temperatureAnomalyMultiplier = 50
-    const temperatureAnomalyBonus = apparentTemperature > temperature+5 ? temperatureAnomalyMultiplier : 0
+    const temperatureAnomalyBonus = apparentTemperature > temperature + 5 ? temperatureAnomalyMultiplier : 0
 
     // clouds
     const cloudMultiplier = 5
-    const cloudBonus = ((100 - cloudCover)/100) * cloudMultiplier
+    const cloudBonus = ((100 - cloudCover) / 100) * cloudMultiplier
 
     // humidity
     const humidityMultiplier = 5
-    const humidityBonus = ((100 - relativeHumidity)/100) * cloudMultiplier
+    const humidityBonus = ((100 - relativeHumidity) / 100) * cloudMultiplier
 
     // weather code (sus)
     const codeMultiplier = 3
-    const codeBonus = ((100 - weatherCode)/100)*codeMultiplier
+    const codeBonus = ((100 - weatherCode) / 100) * codeMultiplier
 
     // wind direction
     const windDirectionMultiplier = 5
@@ -84,12 +84,12 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
     // wind speed
     const windSpeedMultiplier = 5
     const maxAccountedWindSpeed = 25 // (m/s)
-    const windSpeedBonus = (windSpeed10m > maxAccountedWindSpeed ? 0 : (maxAccountedWindSpeed-windSpeed10m)/maxAccountedWindSpeed) * windSpeedMultiplier
+    const windSpeedBonus = (windSpeed10m > maxAccountedWindSpeed ? 0 : (maxAccountedWindSpeed - windSpeed10m) / maxAccountedWindSpeed) * windSpeedMultiplier
 
     // wind gust
     const windGustsMultiplier = 5
     const maxAccountedGustSpeed = 50 // (m/s)
-    const windGustsBonus = (windGusts10m > maxAccountedGustSpeed ? 0 : (maxAccountedGustSpeed-windGusts10m)/maxAccountedGustSpeed) * windGustsMultiplier;
+    const windGustsBonus = (windGusts10m > maxAccountedGustSpeed ? 0 : (maxAccountedGustSpeed - windGusts10m) / maxAccountedGustSpeed) * windGustsMultiplier;
 
     // random deviation
     const deviationMin = -2
@@ -99,10 +99,7 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
     //const eventMultiplier = await getCurrentEventMultiplier();
     const basePluta = -25
 
-    const maxPluty = basePluta + timeMultiplier + dayMultiplier + monthMultiplier + sunlightMultiplier + uvIndexMultiplier + rainMultiplier + showersMultiplier + snowMultiplier + temperatureMultiplier + cloudMultiplier + humidityMultiplier + codeMultiplier + windDirectionMultiplier + windSpeedMultiplier + windGustsMultiplier;
-    const plutaValue = Math.round(basePluta + timeBonus + dayBonus + monthBonus + sunlightBonus + uvIndexBonus + rainBonus + showersBonus + snowBonus + temperatureBonus + temperatureAnomalyBonus + cloudBonus + humidityBonus + codeBonus + windDirectionBonus + windSpeedBonus + windGustsBonus + deviation);
-    //console.log(`Max Plut: ${maxPluty}`)
-    //console.log(`Plut: ${pluty}`)
+    const plutaValue = parseFloat((basePluta + timeBonus + dayBonus + monthBonus + sunlightBonus + uvIndexBonus + rainBonus + showersBonus + snowBonus + temperatureBonus + temperatureAnomalyBonus + cloudBonus + humidityBonus + codeBonus + windDirectionBonus + windSpeedBonus + windGustsBonus + deviation).toFixed(1));
 
     const plutaDev = `
     ## ${plutaValue} Plut
@@ -132,25 +129,25 @@ const getNewPlutaValue = async (latitude: number, longitude: number) => {
 };
 
 const calcTimeFactor = (hour: number, minute: number): number => {
-    const godzina: number =  Number(String(hour)+String(minute < 10 ? "0"+minute : minute))
+    const godzina: number = Number(String(hour) + String(minute < 10 ? "0" + minute : minute))
     let factor: number = 0
 
-    let przerwyBezDlugiej = [[805,815],[900,910],[955,1005],[1050,1100],[1250,1300], [1345,1355],[1440,1450],[1535,1545]]
+    let przerwyBezDlugiej = [[805, 815], [900, 910], [955, 1005], [1050, 1100], [1250, 1300], [1345, 1355], [1440, 1450], [1535, 1545]]
 
     // Between 11:45 and 12:00 is max bonus (long break)
-    if(1145<=godzina && godzina<=1200){
+    if (1145 <= godzina && godzina <= 1200) {
         factor = 1
     }
 
     // 10min before the long break is a big bonus
-    else if((1135<=godzina && godzina<1145) || (1200<godzina && godzina<=1215)){
+    else if ((1135 <= godzina && godzina < 1145) || (1200 < godzina && godzina <= 1215)) {
         factor = 0.5
     }
 
     // on every other break there's a smaller boost
-    else{
-        for(const przerwa of przerwyBezDlugiej){
-            if(przerwa[0] <= godzina && godzina <= przerwa[1]){
+    else {
+        for (const przerwa of przerwyBezDlugiej) {
+            if (przerwa[0] <= godzina && godzina <= przerwa[1]) {
                 factor = 0.25
                 return factor
             }
@@ -161,18 +158,18 @@ const calcTimeFactor = (hour: number, minute: number): number => {
 }
 
 const calcMonthFactor = (month: number): number => ({
-        1: 0.5,
-        2: 0.2,
-        3: 0,
-        4: 0.9,
-        5: 1,
-        6: 1,
-        7: 1,
-        8: 1,
-        9: 0.75,
-        10: 0.5,
-        11: 0,
-        12: 0.85
+    1: 0.5,
+    2: 0.2,
+    3: 0,
+    4: 0.9,
+    5: 1,
+    6: 1,
+    7: 1,
+    8: 1,
+    9: 0.75,
+    10: 0.5,
+    11: 0,
+    12: 0.85
 }[month] || 0);
 
 const calcDayFactor = (day: number): number => ({
@@ -186,8 +183,8 @@ const calcDayFactor = (day: number): number => ({
 }[day] || 0);
 
 const calcTempFactor = (temperature: number): number => {
-    let cieplo: number = -(Math.abs((temperature-20)/20))+1
-    let zimno: number = -(Math.abs((temperature+5)/5))+1
+    let cieplo: number = -(Math.abs((temperature - 20) / 20)) + 1
+    let zimno: number = -(Math.abs((temperature + 5) / 5)) + 1
 
     zimno = zimno > 1 ? 1 : zimno
     cieplo = cieplo > 1 ? 1 : cieplo
