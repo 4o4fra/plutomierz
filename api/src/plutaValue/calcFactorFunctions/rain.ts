a32708a162c6ea90fdec0def41721c88d505a640
Danrejk <SDanrejk@gmail.com>
2024-12-15 22:50:00 +0100

    reduced the amount of rain cap

--- api/src/plutaValue/calcFactorFunctions/rain.ts
+++ api/src/plutaValue/calcFactorFunctions/rain.ts
@@ -1,9 +1,11 @@
+const rainCap = 0.5; // 0.5 mm is enough rain
+
 export default function calcRainFactor(rain: number): number {
    if (rain <= 0) {
         return 1;
-    } else if (rain > 1) {
+    } else if (rain > rainCap) {
         return 0;
     } else {
-        return 1 - rain;
+        return rainCap - rain;
 }
 }
