5e0067b120c1fcc0f41c14fd0cba6daeee6a2f99
4o4fra <franio.grzywaczewski@gmail.com>
2024-12-16 14:18:37 +0100

    :art: Improve code structure

--- api/src/plutaValue/calcFactorFunctions/break.ts
+++ api/src/plutaValue/calcFactorFunctions/break.ts
@@ -1,29 +1,29 @@
-export default function calcBreakFactor (time: number): number {
-    let factor: number = 0
+const LONG_BREAK_START = 1145;
+const LONG_BREAK_END = 1200;
+const ALMOST_LONG_BREAK_START = 1135;
+const ALMOST_LONG_BREAK_END = 1215;
 
-    const longBreakFactorial:number = 1
-    const almostLongBreakFactorial:number = 0.5
-    const shortBreakFactorial:number = 0.25
+const regularBreaks = [
+    [805, 815], [900, 910], [955, 1005], [1050, 1100],
+    [1250, 1300], [1345, 1355], [1440, 1450], [1535, 1545]
+];
 
-    let regularBreaks = [[805, 815], [900, 910], [955, 1005], [1050, 1100], [1250, 1300], [1345, 1355], [1440, 1450], [1535, 1545]]
+export default function calcBreakFactor(time: number): number {
+    const longBreakFactorial = 1;
+    const almostLongBreakFactorial = 0.5;
+    const shortBreakFactorial = 0.25;
 
-    // Between 11:45 and 12:00 is max bonus (long break)
-    if (1145 <= time && time <= 1200) {
-        factor = longBreakFactorial
-    }
-    // 10min before the long break is a big bonus
-    else if ((1135 <= time && time < 1145) || (1200 < time && time <= 1215)) {
-        factor = almostLongBreakFactorial
-    }
-    else {
-        // on every other break there's a smaller boost
-        for (const timeBreak of regularBreaks) {
-            if (timeBreak[0] <= time && time <= timeBreak[1]) {
-                factor = shortBreakFactorial
-                return factor
+    if (LONG_BREAK_START <= time && time <= LONG_BREAK_END) {
+        return longBreakFactorial;
+    } else if ((ALMOST_LONG_BREAK_START <= time && time < LONG_BREAK_START) || (LONG_BREAK_END < time && time <= ALMOST_LONG_BREAK_END)) {
+        return almostLongBreakFactorial;
+    } else {
+        for (const [start, end] of regularBreaks) {
+            if (start <= time && time <= end) {
+                return shortBreakFactorial;
             }
         }
     }
 
-    return factor
+    return 0;
 }
