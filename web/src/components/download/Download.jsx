f3cf17c59e9b96b049384327888df033be9ed018
Beukot <michalek7790@gmail.com>
2024-12-19 09:19:49 +0100

    added placeholder

---
+++ web/src/components/download/Download.jsx
@@ -0,0 +1,12 @@
+import "Download.css"
+
+function Download() {
+
+    return (
+        <div>
+            <img src={"assets/placeholder.png"} alt={"placeholder"}/>
+        </div>
+    )
+}
+
+export default Download
